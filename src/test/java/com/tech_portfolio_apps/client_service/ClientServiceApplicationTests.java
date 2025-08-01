7dae818a940f310f1a1fca13f29af4d460b32845
Ambrose <praveenambrose18@gmail.com>
2025-08-01 22:11:56 +0530

    initial commit

---
+++ src/test/java/com/tech_portfolio_apps/client_service/ClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech_portfolio_apps.client_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
